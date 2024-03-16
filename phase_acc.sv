c706da3f86b13a238a8786d0a2ff5f192544b53d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-03-16 21:10:58 +0100

    phase accumulator

--- phase_acc.sv
+++ phase_acc.sv
@@ -1,1 +1,12 @@
+module phase_acc (
+    input logic clk, reset_n, enable
+    output logic q[7:0]
+);
 
+always_ff @(posedge clk)
+    if(reset_n == 1'b0)
+        q <='0;
+    else if(enable)
+        q <= q + 1'b1;
+
+endmodule
