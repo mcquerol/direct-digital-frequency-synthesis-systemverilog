2fd7e24939fa2653830e3af65546a7db3ba5dd47
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-03-17 20:20:02 +0100

    fixed reset condition to 1'b0 instead of 1

--- time_base_gen.sv
+++ time_base_gen.sv
@@ -1,4 +1,4 @@
-module time_base_gen #(parameter  L = 512)  (
+module time_base_gen #(parameter L = 512)  (
     input logic clk, reset_n,
     output logic q
 );
@@ -7,7 +7,7 @@ localparam BITWIDTH = $clog2(L);
 
 logic [BITWIDTH-1:0] time_base = 0;
 always_ff @(posedge clk)
-    if(reset_n == 1)
+    if(reset_n == 1'b0)
         time_base <= '0;
     else
         time_base <= (time_base + 1'b1) % L;   
